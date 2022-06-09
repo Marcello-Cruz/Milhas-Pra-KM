65a5a4b9e7beba24e7038654c4c4dd3bf0c447c8
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-08 21:43:59 -0300

    add exercicio corrigido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas, kms;
+
+            Console.Write("Entre com a medida (em milhas): ");
+            milhas = Convert.ToDouble(Console.ReadLine());
+
+            kms = milhas * 1.609;
+
+            Console.WriteLine($"{kms:N3} Km");
+        }
+    }
+}
